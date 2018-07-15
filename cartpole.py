ceb0e039814b3d16edf1e956ee734e51e0534867
Swarna <swylye@gmail.com>
2018-07-15 17:46:15 +0800

    Reinforcement learning using random search method

--- cartpole.py
+++ cartpole.py
@@ -1,12 +1,34 @@
+import numpy as np
 import gym
+
 env = gym.make('CartPole-v0')
-for i_episode in range(20):
+
+# parameters = np.random.rand(4)
+
+def run_episode(env, parameters):  
     observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
+    totalreward = 0
+    for _ in range(200):
+        action = 0 if np.matmul(parameters,observation) < 0.5 else 1
         observation, reward, done, info = env.step(action)
+        totalreward += reward
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            # print('Total rewards: {}'.format(totalreward))
+            # print('Parameters used: {}'.format(parameters))
+            break
+    return totalreward
+
+bestparams = None  
+bestreward = 0  
+for _ in range(10000):  
+    parameters = np.random.rand(4)
+    reward = run_episode(env,parameters)
+    if reward > bestreward:
+        print('Parameters updated at {} round.'.format(_+1))
+        print('We managed to last for {} timesteps!'.format(reward))
+        bestreward = reward
+        bestparams = parameters
+        # considered solved if the agent lasts 200 timesteps
+        if reward == 200:
+            print(bestparams)
             break
